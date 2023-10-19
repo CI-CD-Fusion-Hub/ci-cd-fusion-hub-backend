9931c68d89dd320f85604ce282fc7ae1c5ca6ef3
ivan.chalakov <ivan.chalakov@alpha-devs.com>
2023-10-19 14:04:49 +0300

    add out type to status route

--- routers/status_rt.py
+++ routers/status_rt.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
+from schemas.response_sch import Response
 from utils.response import ok
 
 router = APIRouter()
 
 
 @router.get("/status", tags=["status"])
-async def status():
+async def status() -> Response:
     return ok(message="Service is healthy!")
