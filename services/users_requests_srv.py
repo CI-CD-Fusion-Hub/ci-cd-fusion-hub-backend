a54d90be6aab2973be6c91b61612b484289686a1
ivan.chalakov <ivan.chalakov@alpha-devs.com>
2023-11-21 22:47:53 +0200

    add logs in services/users_requests_srv.py

--- services/users_requests_srv.py
+++ services/users_requests_srv.py
@@ -1,11 +1,12 @@
-from fastapi import Request
 from fastapi import status as Status
 
 from daos.users_requests_dao import UserRequestsDAO
-from schemas.users_requests_sch import CreateUsersRequest, UpdateUsersRequest, Pipeline, \
+from schemas.users_requests_sch import UpdateUsersRequest, Pipeline, \
     UsersRequestOut, User
-from utils.enums import SessionAttributes, AccessLevel, RequestStatus
+from utils.logger import Logger
 from utils.response import ok, error
+
+LOGGER = Logger().start_logger()
 
 
 class UsersRequestsService:
@@ -15,6 +16,10 @@ class UsersRequestsService:
     async def get_all_users_requests(self):
         users_requests = await self.user_requests_dao.get_all()
 
+        if not users_requests:
+            LOGGER.info("No user requests found in the database.")
+            return ok(message="No user requests available.", data=[])
+
         users_requests_data = []
         for user_request in users_requests:
             users_request_data = UsersRequestOut.model_validate(user_request.as_dict())
@@ -23,6 +28,7 @@ class UsersRequestsService:
                                             for pipeline in user_request.pipelines if pipeline.pipeline]
             users_requests_data.append(users_request_data)
 
+        LOGGER.info(f"Retrieved {len(users_requests_data)} user requests.")
         return ok(
             message="Successfully provided all users requests.",
             data=[user_requests
@@ -32,6 +38,7 @@ class UsersRequestsService:
     async def get_by_id(self, users_request_id: int):
         user_request = await self.user_requests_dao.get_detailed_users_request_info_by_id(users_request_id)
         if not user_request:
+            LOGGER.warning(f"User request with ID {users_request_id} not found.")
             return error(
                 message=f"User request with ID {users_request_id} does not exist.",
                 status_code=Status.HTTP_400_BAD_REQUEST
@@ -40,6 +47,8 @@ class UsersRequestsService:
         users_request_data = UsersRequestOut.model_validate(user_request.as_dict())
         users_request_data.pipelines = [(Pipeline(name=pipeline.pipeline.name, id=pipeline.pipeline.id))
                                         for pipeline in user_request.pipelines if pipeline.pipeline]
+
+        LOGGER.info(f"Successfully retrieved user request details for ID {users_request_id}.")
         return ok(
             message="Successfully provided users request details.",
             data=users_request_data
@@ -48,6 +57,7 @@ class UsersRequestsService:
     async def update_users_request(self, users_request_id: int, users_request_data: UpdateUsersRequest):
         user_request = await self.user_requests_dao.get_detailed_users_request_info_by_id(users_request_id)
         if not user_request:
+            LOGGER.warning(f"Attempted to update a non-existent user request with ID {users_request_id}.")
             return error(
                 message=f"User request with ID {users_request_id} does not exist.",
                 status_code=Status.HTTP_400_BAD_REQUEST
@@ -55,15 +65,20 @@ class UsersRequestsService:
 
         updated_data = users_request_data.model_dump(exclude={'pipelines'})
         await self.user_requests_dao.update(users_request_id, updated_data)
+
+        LOGGER.info(f"User request with ID {users_request_id} has been successfully updated.")
         return ok(message="Successfully updated user request.")
 
     async def delete_users_request(self, users_request_id: int):
         user_request = await self.user_requests_dao.get_by_id(users_request_id)
         if not user_request:
+            LOGGER.warning(f"Attempted to delete a non-existent user request with ID {users_request_id}.")
             return error(
                 message=f"User request with ID {users_request_id} does not exist.",
                 status_code=Status.HTTP_400_BAD_REQUEST
             )
 
         await self.user_requests_dao.delete(users_request_id)
+        LOGGER.info(f"User request with ID {users_request_id} has been successfully deleted.")
+
         return ok(message="User request has been successfully deleted.")
